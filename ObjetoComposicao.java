f7023fe55749587e197077b1d9440454682de20c
Julia Souza <juju.souza09@yahoo.com>
2025-10-29 08:38:45 -0300

    Add main method to demonstrate Livro class

--- ObjetoComposicao.java
+++ ObjetoComposicao.java
@@ -2,4 +2,10 @@ package Aula11;
 
 public class ObjetoComposicao {
 
+  public static void main(String[] args) {
+		
+        Livro l1 = new Livro("O Sol é para Todos", "Harper Lee");
+        l1.mostrarInfo();
+    }
+
 }
