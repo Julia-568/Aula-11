2fd0160b4d189bf4b2ccca03f828ad23a44a842b
Julia Souza <juju.souza09@yahoo.com>
2025-10-29 08:37:41 -0300

    Add title and author fields to Livro class

--- Livro.java
+++ Livro.java
@@ -2,4 +2,17 @@ package Aula11;
 
 public class Livro {
 
+  private String titulo;
+  private Autor autor;
+
+    public Livro(String titulo, String nomeAutor) {
+        this.titulo = titulo;
+        this.autor = new Autor(nomeAutor);
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Livro: " + titulo);
+        System.out.println("Autor: " + autor.getNome());
+    }
+
 }
