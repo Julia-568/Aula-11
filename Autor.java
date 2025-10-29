b8c21053adf0560db402d766591ad62a89573793
Julia Souza <juju.souza09@yahoo.com>
2025-10-29 08:37:07 -0300

    Add Autor class with name property and getter

--- Autor.java
+++ Autor.java
@@ -2,4 +2,14 @@ package Aula11;
 
 public class Autor {
 
+  private String nome;
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 }
